6e7bd88a86a58da6397db14796128d74a9008597
Andrew Phommathep <13destinies@gmail.com>
2016-02-24 10:52:24 -0800

    2 Create pages.about

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('about', function () {
+    return view('pages.about'); // resources/views/pages/about.blade.php
+});
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
