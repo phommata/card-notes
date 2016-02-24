db394209d19ed7d22e45a2b9bcdf349303d846a4
Andrew Phommathep <13destinies@gmail.com>
2016-02-24 14:01:00 -0800

    5 Layout Files

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('header')
+    <link>
+
+@section('content')
+    <h1>The About Page Goes Here</h1>
+@stop
+
+@section('footer')
+    <script>
+        alert('About page only');
+    </script>
+@stop
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    @yield('header')
+</head>
+<body>
+
+    @yield('content')
+
+    @yield('footer')
+</body>
+</html>
