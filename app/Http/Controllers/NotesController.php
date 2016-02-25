e2dc7f2ed8b7349e718e92d33ea4ea6b935c55f8
Andrew Phommathep <13destinies@gmail.com>
2016-02-25 10:18:43 -0800

    9 Forms
    Explicit - Adding a Note to a Card.

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -13,23 +13,9 @@ class NotesController extends Controller
 {
     public function store(Request $request, Card $card)
     {
-//        return $card;
-
-//        $note = new Note;
-//
-//        $note->body = $request->body;
-//
-//        $card->notes()->save($note);
-
-//        $card->notes()->save(
-//            new Note(['body' => $request->body])
-//        );
-
-//        $card->notes()->create([
-//            'body' => $request->body
-//        ]);
-
-        $card->notes()->create($request->all()); // []
+        $card->addNote(
+            new Note($request->all())
+        );
 
 //        return \Redirect::to('/some/new/uri');
 //        return redirect()->to('/some/new/uri');
