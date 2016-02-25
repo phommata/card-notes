ddcdd28e597be885ce0d64e2aeb4355ec4fc65d3
Andrew Phommathep <13destinies@gmail.com>
2016-02-25 10:08:07 -0800

    9 Forms
    Redirect

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -4,14 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
+//use App\Http\Requests;
+//use App\Http\Controllers\Controller;
+use App\Note;
+use App\Card;
 
 class NotesController extends Controller
 {
-    public function store()
+    public function store(Request $request, Card $card)
     {
-//        return \Request::all();
-        return request()->all();
+//        return $card;
+
+        $note = new Note;
+
+        $note->body = $request->body;
+
+        $card->notes()->save($note);
+
+//        return \Redirect::to('/some/new/uri');
+//        return redirect()->to('/some/new/uri');
+//        return redirect('/some/new/uri');
+        return back();
     }
 }
